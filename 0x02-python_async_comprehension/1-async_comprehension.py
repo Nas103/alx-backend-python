c0b97c420a0e0651e6614baf2f32cd74171b3bd3
Nas103 <138734910+Nas103@users.noreply.github.com>
2024-10-15 15:40:50 +0200

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Module that defines an async comprehension coroutine."""
-
 import asyncio
-from typing import List
-from .async_generator import async_generator
 
+from .0_async_generator import async_generator  # Import from previous task
 
-async def async_comprehension() -> List[float]:
-    """Collect 10 random numbers using an async comprehension
-    over async_generator and return the numbers.
+async def async_comprehension():
     """
-    return [i async for i in async_generator()]
+    An asynchronous coroutine that collects 10 random numbers using an
+    async comprehension over async_generator and returns them as a list.
+    """
+
+    random_numbers = [num async for num in async_generator()]
+    return random_numbers
